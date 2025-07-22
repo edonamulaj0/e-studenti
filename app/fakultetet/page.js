156cde673a02bfc925d4701540405edc6423b5c8
Dona <edonaamulaj@gmail.com>
2025-07-22 15:29:23 +0200

    commit

--- app/fakultetet/page.js
+++ app/fakultetet/page.js
@@ -232,7 +232,7 @@ export default function Faculties() {
                 <div className="flex items-center">
                   <Users className="w-5 h-5 text-gray-500 mr-2" />
                   <span className="text-sm text-gray-600">
-                    {faculty.students} Studentët
+                    {faculty.students} Studentë
                   </span>
                 </div>
               </div>
