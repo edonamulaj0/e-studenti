d28d1cc12ef104de7f81b03e7c36325cc4085559
Dona <edonaamulaj@gmail.com>
2025-07-23 13:28:09 +0200

    pages, erasmus

--- app/page.js
+++ app/page.js
@@ -121,9 +121,9 @@ export default function Home() {
       </section>
 
       {/* Future Features */}
-      <section className="py-16 bg-gray-50">
+      {/* <section className="py-16 bg-gray-50">
         <div className="container mx-auto px-4">
-          {/* <h2 className="text-3xl font-bold text-center mb-12">Së shpejti!</h2> */}
+          <h2 className="text-3xl font-bold text-center mb-12">Së shpejti!</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="bg-white rounded-xl shadow-lg p-6 opacity-75">
               <h3 className="text-xl font-semibold mb-2">Cka është Erasmus?</h3>
@@ -136,7 +136,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
