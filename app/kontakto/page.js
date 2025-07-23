24eb40c973b37d821281fff656fc26b7a9c4d64b
Dona <edonaamulaj@gmail.com>
2025-07-23 13:35:40 +0200

    commit

--- app/kontakto/page.js
+++ app/kontakto/page.js
@@ -81,7 +81,7 @@ export default function Kontakto() {
                   <textarea
                     value="Lënda - Semestri - Profesori"
                     readOnly
-                    rows={5}
+                    rows={3}
                     className="w-full px-3 py-2 bg-gray-50 border rounded text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none"
                   />
                 </div>
